d122d1b89c49b5a5f843cf2a871bcf8451a2964f
roivasxxx <tiragoshi@gmail.com>
2023-05-01 11:09:22 +0200

    display alive birds

--- sketch.js
+++ sketch.js
@@ -20,66 +20,69 @@ let generation = 1;
 let generationSpan;
 
 function preload() {
-	birdImg = loadImage("assets/bird.png");
-	pipeTopImg = loadImage("assets/pipeUp.png");
-	pipeDownImg = loadImage("assets/pipeDown.png");
-	bg = loadImage("assets/bg.png");
-	groundImg = loadImage("assets/ground.png");
+  birdImg = loadImage("assets/bird.png");
+  pipeTopImg = loadImage("assets/pipeUp.png");
+  pipeDownImg = loadImage("assets/pipeDown.png");
+  bg = loadImage("assets/bg.png");
+  groundImg = loadImage("assets/ground.png");
 }
 
 function setup() {
-	let canvas = createCanvas(bg.width, bg.height);
-	generationSpan = select("#generation");
-	generationSpan.html(generation);
-	canvas.parent("sketch");
-	for (let i = 0; i < totalPopulation; i++) {
-		let bird = new Bird();
-		aliveBirds[i] = bird;
-		allBirds[i] = bird;
-	}
+  let canvas = createCanvas(bg.width, bg.height);
+  generationSpan = select("#generation");
+  generationSpan.html(generation);
+  birdsAliveSpan = select("#birds-alive");
+  birdsAliveSpan.html(aliveBirds);
+  canvas.parent("sketch");
+  for (let i = 0; i < totalPopulation; i++) {
+    let bird = new Bird();
+    aliveBirds[i] = bird;
+    allBirds[i] = bird;
+  }
 }
 
 function draw() {
-	image(bg, 0, 0);
-	for (let i = pipes.length - 1; i >= 0; i--) {
-		pipes[i].update();
-		if (pipes[i].checkOffScreen()) {
-			pipes.splice(i, 1);
-		}
-	}
+  image(bg, 0, 0);
+  for (let i = pipes.length - 1; i >= 0; i--) {
+    pipes[i].update();
+    if (pipes[i].checkOffScreen()) {
+      pipes.splice(i, 1);
+    }
+  }
 
-	for (let i = aliveBirds.length - 1; i >= 0; i--) {
-		let bird = aliveBirds[i];
-		bird.chooseAction(pipes);
-		bird.update();
-		for (let j = 0; j < pipes.length; j++) {
-			if (pipes[j].checkCollision(bird)) {
-				aliveBirds.splice(i, 1);
-				break;
-			}
-		}
-		if (bird.bottomTopCollision()) {
-			aliveBirds.splice(i, 1);
-		}
-	}
+  for (let i = aliveBirds.length - 1; i >= 0; i--) {
+    let bird = aliveBirds[i];
+    bird.chooseAction(pipes);
+    bird.update();
+    for (let j = 0; j < pipes.length; j++) {
+      if (pipes[j].checkCollision(bird)) {
+        aliveBirds.splice(i, 1);
+        break;
+      }
+    }
+    if (bird.bottomTopCollision()) {
+      aliveBirds.splice(i, 1);
+      birdsAliveSpan.html(aliveBirds);
+    }
+  }
 
-	if (frameCounter % 50 === 0) {
-		pipes.push(new Pipe());
-	}
+  if (frameCounter % 50 === 0) {
+    pipes.push(new Pipe());
+  }
 
-	frameCounter++;
+  frameCounter++;
 
-	for (let i = 0; i < pipes.length; i++) {
-		pipes[i].show();
-	}
-	for (let i = 0; i < aliveBirds.length; i++) {
-		aliveBirds[i].show();
-	}
-	if (aliveBirds.length == 0) {
-		generation++;
-		generationSpan.html(generation);
-		createNextGeneration();
-	}
+  for (let i = 0; i < pipes.length; i++) {
+    pipes[i].show();
+  }
+  for (let i = 0; i < aliveBirds.length; i++) {
+    aliveBirds[i].show();
+  }
+  if (aliveBirds.length == 0) {
+    generation++;
+    generationSpan.html(generation);
+    createNextGeneration();
+  }
 
-	image(groundImg, 0, height - groundImg.height);
+  image(groundImg, 0, height - groundImg.height);
 }
